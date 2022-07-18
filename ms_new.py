a01489485b6cac51a670db1f4227f8db2a99d13e
Kyuu <kyuu@github.com>
2022-07-19 00:32:52 +0800

    Update

--- ms_new.py
+++ ms_new.py
@@ -13,39 +13,51 @@ while(True):
     if(auto_nav_pos):
         print("正在进行自动寻路")
         sleep(2)
+        free_count = 0
         continue
 
 
 
-#     mission_pos = exists(Template(r"tpl1658155662997.png", record_pos=(-0.427, -0.122), resolution=(1280, 720)))
+    if(free_count > 10):
+        
+        mission_pos = exists(Template(r"tpl1658155662997.png", record_pos=(-0.427, -0.122), resolution=(1280, 720)))
 
-#     if(mission_pos):
-#         touch(mission_pos)
+        if(mission_pos):
+            touch(mission_pos)
+            print("主动点击任务")
+            free_count = 0
+            continue
+            
     next_pos = exists(Template(r"tpl1658155992222.png", record_pos=(0.448, 0.112), resolution=(1280, 720)))
     if(next_pos):
         touch(next_pos)
         print("点击下一步")
+        free_count = 0
         continue
     
     accept_pos = exists(Template(r"tpl1658156074983.png", record_pos=(0.362, 0.045), resolution=(1280, 720)))
     if(accept_pos):
         touch(accept_pos)
         print("点击接受")
+        free_count = 0
         continue
         
     finish_pos = exists(Template(r"tpl1658156339504.png", record_pos=(0.362, 0.046), resolution=(1280, 720)))
     if(finish_pos):
         touch(finish_pos)
         print("点击完成")
+        free_count = 0
         continue
     
     ok_pos =  exists(Template(r"tpl1658156596398.png", record_pos=(0.366, 0.047), resolution=(1280, 720)))
     if(ok_pos):
         touch(ok_pos)
         print("点击确认")
+        free_count = 0
         continue
         
-    award_pos = exists(Template(r"tpl1658155757756.png", record_pos=(-0.003, 0.221), resolution=(1280, 720)))
+    award_pos = exists(Template(r"tpl1658160944184.png", record_pos=(0.004, 0.22), resolution=(1280, 720)))
+
     if(award_pos):
         touch(award_pos)
         print("领取奖励")
@@ -55,19 +67,35 @@ while(True):
     auto_skill_pos = exists(Template(r"tpl1658156481752.png", record_pos=(0.116, -0.009), resolution=(1280, 720)))
     if(auto_skill_pos):
         touch(auto_skill_pos)
+        free_count = 0
         print("自动加点")
+        continue
     
     auto_equip_pos = exists(Template(r"tpl1658157013067.png", record_pos=(0.101, 0.026), resolution=(1280, 720)))
     if(auto_equip_pos):
         touch(auto_equip_pos)
         print("自动装备")
+        free_count = 0
+        continue
     
     light_pos = exists(Template(r"tpl1658157548248.png", record_pos=(-0.001, 0.084), resolution=(1280, 720)))
     if(light_pos):
         touch(light_pos)
-        print("点击附近任务")
+        print("点击附近任务灯泡")
+        free_count = 0
+        continue
+        
+    maple_book_pos = exists(Template(r"tpl1658159739966.png", record_pos=(0.151, 0.077), resolution=(1280, 720)))
+    if(maple_book_pos):
+        touch(maple_book_pos)
+        print("点击附近任务枫叶书")
+        free_count = 0
+        continue
+    
     
     free_count+=1
+    print("空置 %d 回" %(free_count))
+
    
     
         
