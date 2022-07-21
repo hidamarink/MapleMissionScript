c706157e2acf9d5b398f67239278e96a61c2394a
Kyuu <kyuu@github.com>
2022-07-21 10:09:56 +0800

    WaitTime Configable

--- ms_new.py
+++ ms_new.py
@@ -7,6 +7,7 @@ auto_setup(__file__)
 
 free_count = 0
 light_lock = False
+wait_timeout = 1
 auto_task_temp = Template(r"tpl1658164859868.png", record_pos=(-0.169, 0.228), resolution=(1280, 720))
 auto_nav_temp = Template(r"tpl1658156262845.png", record_pos=(-0.087, -0.125), resolution=(1280, 720))
 mission_temp = Template(r"tpl1658155662997.png", record_pos=(-0.427, -0.122), resolution=(1280, 720))
@@ -39,26 +40,26 @@ while(True):
     auto_task_pos = auto_task_temp.match_in(screen)
     if(auto_task_pos):
         print("检测到正在执行自动任务 等待")
-        sleep(2)
+        sleep(wait_timeout)
         free_count = 0
         continue
 
     auto_nav_pos = auto_nav_temp.match_in(screen)
     if(auto_nav_pos):
         print("正在进行自动寻路")
-        sleep(2)
+        sleep(wait_timeout)
         free_count = 0
         continue
 
     map_loading_pos = map_loading_temp.match_in(screen)
     if(map_loading_pos):
         print("过图加载中。。。")
-        sleep(2)
+        sleep(wait_timeout)
         free_count = 0
         continue
 
 
-    if(free_count >= 5):
+    if(free_count >= 3):
         print("空置回数到达 %d 回 寻找新任务" %(free_count))
         mission_pos = mission_temp.match_in(screen)
         if(mission_pos):
@@ -72,7 +73,7 @@ while(True):
     if(next_pos):
         touch(next_pos)
         print("点击下一步")
-        sleep(2)
+        sleep(wait_timeout)
         free_count = 0
         continue
     
@@ -81,7 +82,7 @@ while(True):
         touch(accept_pos)
         print("点击接受")
         free_count = 0
-        sleep(2)
+        sleep(wait_timeout)
         continue
         
     finish_pos = finish_temp.match_in(screen)
@@ -89,7 +90,7 @@ while(True):
         touch(finish_pos)
         print("点击完成")
         free_count = 0
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     ok_pos = ok_temp.match_in(screen)
@@ -97,7 +98,7 @@ while(True):
         touch(ok_pos)
         print("点击确认")
         free_count = 0
-        sleep(2)
+        sleep(wait_timeout)
         continue
         
     award_pos = award_temp.match_in(screen)
@@ -105,7 +106,7 @@ while(True):
     if(award_pos):
         touch(award_pos)
         print("领取奖励")
-        sleep(2)
+        sleep(wait_timeout)
         continue
 
     
@@ -114,7 +115,7 @@ while(True):
         touch(auto_skill_pos)
         free_count = 0
         print("自动加点")
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     auto_equip_pos = auto_equip_temp.match_in(screen)
@@ -122,7 +123,7 @@ while(True):
         touch(auto_equip_pos)
         print("自动装备")
         free_count = 0
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     light_pos = light_temp.match_in(screen)
@@ -134,7 +135,7 @@ while(True):
             print("点击附近任务灯泡")
             light_lock = True
             free_count = 1
-            sleep(2)
+            sleep(wait_timeout)
             continue
         
     maple_book_pos = maple_book_temp.match_in(screen)
@@ -142,7 +143,7 @@ while(True):
         touch(maple_book_pos)
         print("点击附近任务枫叶书")
         free_count = 0
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     start_mission_pos = start_mission_temp.match_in(screen)
@@ -150,20 +151,20 @@ while(True):
     if(finish_mission_pos):
         touch(finish_mission_pos)
         print("点击结束任务")
-        sleep(2)
+        sleep(wait_timeout)
         continue
         
     if(start_mission_pos):
         touch(start_mission_pos)
         print("多选任务开始")
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     hot_and_new_pos = hot_and_new_temp.match_in(screen)
     if(hot_and_new_pos):
         print("关闭广告 HOT & NEW")
         touch([1143,70])
-        sleep(2)
+        sleep(wait_timeout)
         continue
     
     free_count+=1
